de6d17e564a403742084719335d7f098b8a2cc3d
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-04-24 15:30:52 -0700

    Color Scheme

---
+++ IR_Black_matlab.m
@@ -0,0 +1,48 @@
+% IR_Black Color Theme for MATLAB
+% Ported to MATLAB by Xiaoshan Huang, xiaoshanhuang@gmail.com
+% 
+% For more information, see http://toddwerth.com/2011/07/21/the-original-ir_black-for-os-x-lion/
+
+com.mathworks.services.Prefs.setBooleanPref('ColorsUseSystem', 0);
+
+com.mathworks.services.Prefs.setColorPref('ColorsText', java.awt.Color(int32(246), int32(247), int32(238)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsText');
+
+com.mathworks.services.Prefs.setColorPref('ColorsBackground', java.awt.Color(int32(29), int32(30), int32(25)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsBackground');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Keywords', java.awt.Color(int32(133), int32(190), int32(254)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Keywords');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Comments', java.awt.Color(int32(55), int32(155), int32(78)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Comments');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Strings', java.awt.Color(int32(185), int32(181), int32(253)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Strings');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_UnterminatedStrings', java.awt.Color(int32(252), int32(81), int32(252)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_UnterminatedStrings');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_SystemCommands', java.awt.Color(int32(255), int32(255), int32(168)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_SystemCommands');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Errors', java.awt.Color(int32(253), int32(84), int32(78)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Errors');
+
+com.mathworks.services.Prefs.setColorPref('Colors_HTML_HTMLLinks', java.awt.Color(int32(133), int32(190), int32(254)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_HTML_HTMLLinks');
+
+com.mathworks.services.Prefs.setColorPref('Color_CmdWinWarnings', java.awt.Color(int32(255), int32(165), int32(96)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Color_CmdWinWarnings');
+
+com.mathworks.services.Prefs.setColorPref('Color_CmdWinErrors', java.awt.Color(int32(253), int32(84), int32(78)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Color_CmdWinErrors');
+
+com.mathworks.services.Prefs.setColorPref('Editorhighlight-lines', java.awt.Color(int32(34), int32(35), int32(30)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Editorhighlight-lines');
+
+com.mathworks.services.Prefs.setColorPref('Editor.VariableHighlighting.Color', java.awt.Color(int32(53), int32(56), int32(127)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Editor.VariableHighlighting.Color');
+
+com.mathworks.services.Prefs.setColorPref('ColorsMLintAutoFixBackground', java.awt.Color(int32(102), int32(51), int32(0)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsMLintAutoFixBackground');
