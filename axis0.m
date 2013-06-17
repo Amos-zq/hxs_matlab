f3432999d292aadf87c0319342b751f2ec965f77
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-06-17 16:15:19 -0700

    axis0

---
+++ axis0.m
@@ -0,0 +1,5 @@
+alpha = 0;
+hx = graph2d.constantline(0, 'LineStyle','-', 'Color',alpha*[1 1 1]);
+changedependvar(hx,'x');
+hy = graph2d.constantline(0, 'LineStyle','-', 'Color',alpha*[1 1 1]);
+changedependvar(hy,'y');
