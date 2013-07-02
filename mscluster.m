fe194d93b0b2187294f6f28f49db5aad51cd3542
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-07-01 18:33:31 -0700

    optimize input params

--- mscluster.m
+++ mscluster.m
@@ -1,4 +1,4 @@
-function [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, chanlocs, sm_r, sm_alpha, b, l, bcg)
+function [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, ts, ss, bcg)
 %mscluster EEG microstate clustering analysis
 % R. D. Pascual-Marqui, C. M. Michel, and D. Lehmann, ?Segmentation of
 % brain electrical activity into microstates: model estimation and
@@ -7,17 +7,14 @@ function [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, chanlocs
 %
 % 
 % Usage:
-%     [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, chanlocs, sm_r, sm_alpha, b, l);
+%     [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, ts, ss, bcg);
 % 
 % Inputs:
 %     V:          EEG data, channel*frame
 %     Nu:         number of clusters
 %     iterN:      number of iteration
-%     chanlocs:   chanlocs for spatial smoothness
-%     sm_r:       spatial smoothness range
-%     sm_alpha:   spatial smoothness ratio
-%     b:          time smoothness range
-%     l:          time smoothness ratio
+%     ts:         time smoothness range and ratio [ts_b ts_l]
+%     ss:         spatial smoothness kernal ss_alpha*ss_K
 %     bcg:        bcg noise
 % 
 % Outputs:
@@ -35,9 +32,40 @@ function [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, chanlocs
 %     v0.2:   24-Apr-2013, revesion
 %     v0.3:   21-May-2013, optimize relabeling
 %     v0.4:   22-May-2013, add bcg
+%     v0.5:   01-Jul-2013, optimize input params
 %     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if nargin < 4
+    ts = []; ss = []; bcg = [];
+end
+if nargin < 5
+    ss = []; bcg = [];
+end
+if nargin < 6
+    bcg = [];
+end
 
+if isempty(ts)
+    time_smooth = 0;
+else
+    time_smooth = 1;
+    ts_b = ts(1);
+    ts_l = ts(2);
+end
+
+if isempty(ss)
+    spatial_smooth = 0;
+else
+    spatial_smooth = 1;
+    ss_K = ss;
+end
+if isempty(bcg)
+    bcg_reduction = 0;
+else
+    bcg_reduction = 1;
+    bcg = bcg - repmat(mean(bcg,2), 1, size(bcg,2));
+    ECovRes = bcg*bcg'/size(bcg,2);
+end
 
 % init
 [Ns,Nt] = size(V);
@@ -45,12 +73,8 @@ function [L, Gamma, alpha, R, sigma_mcv, log] = mscluster(V, Nu, iterN, chanlocs
 L = randi(Nu,[Nt 1]);
 Gamma = zeros(Ns, Nu);
 
-K = sm_k(chanlocs, sm_r);
-% bcg = bcg - repmat(mean(bcg,2), 1, size(bcg,2));
-
 eps0 = 10^-6;
 sigma_0 = 0; 
-ECovRes = bcg*bcg'/size(bcg,2);
 % Initialize progress indicator
 nSteps = 20;
 step = 0;
@@ -61,14 +85,20 @@ for n = 1:iterN
     % Calculate Gamma
     for k = 1:Nu
         S = (V(:,L==k)*V(:,L==k)'/sum(L==k));
-%         S = (1+sm_alpha*K)\(V(:,L==k)*V(:,L==k)'/sum(L==k));
-%         Wh = ECovRes^(-0.5);
-%         S = (Wh*V(:,L==k))*(Wh*V(:,L==k))'/sum(L==k);
+        if spatial_smooth
+            S = (1+ss_K)\(V(:,L==k)*V(:,L==k)'/sum(L==k));
+        end
+        if bcg_reduction
+            Wh = ECovRes^(-0.5);
+            S = (Wh*V(:,L==k))*(Wh*V(:,L==k))'/sum(L==k);
+        end
         
         [X,D] = eig(S);
         [Q,I] = sort(diag(D), 'descend');
         Gamma(:,k) = X(:,I(1));
-%         Gamma(:,k) = ECovRes^(0.5) * X(:,I(1));
+        if bcg_reduction
+            Gamma(:,k) = ECovRes^(0.5) * X(:,I(1));
+        end
     end
     sigma_u = sum((sum(V.^2, 1)-sum(Gamma(:,L).*V,1).^2)/(Nt*(Ns-1)), 2);
     if abs(sigma_0-sigma_u) <= eps*sigma_u
@@ -84,38 +114,40 @@ end
 % Deinitialize progress indicator
 fprintf(1, '\n');
 
-% % relable
-% sigma_0 = 0;
-% % Initialize progress indicator
-% nSteps = 20;
-% step = 0;
-% fprintf(1, 'relabeling: |');
-% strLength = fprintf(1, [repmat(' ', 1, nSteps - step) '|   0%%']);
-% tic
-% for n = 1:iterN 
-%     e = sum((sum(V.^2, 1)-sum((Gamma(:,L).*V).^2, 1))/(Nt*(Ns-1)), 2);
-%     Nbkt = zeros(Nt, 2*b+1);
-%     for shift = -b:b
-%         Nbkt(:,shift+b+1) = circshift(L,shift);
-%     end
-%     argk = zeros(Nt,Nu);
-%     for k = 1:Nu
-%         argk(:,k) = ((sum(V.^2, 1)-sum((Gamma(:,k)'*V).^2, 1))/(2*e*(Ns-1)))'...
-%                     - l*sum(Nbkt==k,2);
-%     end
-%     [Y,L] = min(argk, [], 2);
-%     sigma_u = sum((sum(V.^2, 1)-sum(Gamma(:,L).*V,1).^2)/(Nt*(Ns-1)), 2);
-%     if abs(sigma_0-sigma_u) <= eps0*sigma_u
-%         fprintf(1,['\n relabel iter stops at ' num2str(n) '\n']);
-%         break;
-%     else
-%         log(n,2) = abs(sigma_0-sigma_u);
-%         sigma_0 = sigma_u;
-%     end
-%     [step, strLength] = mywaitbar(n, iterN, step, nSteps, strLength);
-% end
-% % Deinitialize progress indicator
-% fprintf(1, '\n');
+if time_smooth
+    % relable
+    sigma_0 = 0;
+    % Initialize progress indicator
+    nSteps = 20;
+    step = 0;
+    fprintf(1, 'relabeling: |');
+    strLength = fprintf(1, [repmat(' ', 1, nSteps - step) '|   0%%']);
+    tic
+    for n = 1:iterN 
+        e = sum((sum(V.^2, 1)-sum((Gamma(:,L).*V).^2, 1))/(Nt*(Ns-1)), 2);
+        Nbkt = zeros(Nt, 2*ts_b+1);
+        for shift = -ts_b:ts_b
+            Nbkt(:,shift+ts_b+1) = circshift(L,shift);
+        end
+        argk = zeros(Nt,Nu);
+        for k = 1:Nu
+            argk(:,k) = ((sum(V.^2, 1)-sum((Gamma(:,k)'*V).^2, 1))/(2*e*(Ns-1)))'...
+                        - ts_l*sum(Nbkt==k,2);
+        end
+        [Y,L] = min(argk, [], 2);
+        sigma_u = sum((sum(V.^2, 1)-sum(Gamma(:,L).*V,1).^2)/(Nt*(Ns-1)), 2);
+        if abs(sigma_0-sigma_u) <= eps0*sigma_u
+            fprintf(1,['\n relabel iter stops at ' num2str(n) '\n']);
+            break;
+        else
+            log(n,2) = abs(sigma_0-sigma_u);
+            sigma_0 = sigma_u;
+        end
+        [step, strLength] = mywaitbar(n, iterN, step, nSteps, strLength);
+    end
+    % Deinitialize progress indicator
+    fprintf(1, '\n');
+end
 
 sigma_mcv = sigma_u*((Ns-1)^-1*(Ns-1-Nu))^-2;
 
@@ -134,26 +166,6 @@ end
 R = 1 - sigma_u/sum(sum(V.^2, 1)/(Nt*(Ns-1)), 2);
 R = sqrt(R);
 
-function K = sm_k(chanlocs, sm_r)
-    % spatial smoothness
-    nc = size(chanlocs,2);
-    chanCor = zeros(6,nc);
-    for i = 1:nc
-        chanCor(:,i) = [chanlocs(i).Y; chanlocs(i).X; chanlocs(i).Z; chanlocs(i).sph_theta; chanlocs(i).sph_phi; chanlocs(i).sph_radius];
-    end
-    radius = mean(chanCor(6,:),2);
-    G = zeros(nc,nc); Diag = G;
-    for i = 1:nc
-        for j = 1:nc
-    %         G(i,j) = exp(-(1/2)*(sum((chanCor(1:3,i)-chanCor(1:3,j)).^2)/(r^2)));
-            G(i,j) = exp(-(1/2)*(distance(chanCor(4,i),chanCor(5,i),chanCor(4,j),chanCor(5,j),radius)^2)/(sm_r^2));
-        end
-    end
-    for i = 1:nc
-        Diag(i,i) = sum(G(i,:));
-    end;
-    K = Diag - G;    
-end
 
 function [step, strLength] = mywaitbar(compl, total, step, nSteps, strLength)
 
