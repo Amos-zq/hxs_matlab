beed80afd55f25b9a1433795f53e628276f3fe22
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-09-03 11:07:04 +0800

    checkerboard stimulus

---
+++ Stimulus/checkerboard_EEG_fMRI.m
@@ -0,0 +1,162 @@
+function checkerboard_EEG_fMRI( shape,trialTime,trialNum,blockNum,cbSize )
+% checkerboard stimulus for EEG-fMRI
+% checkerboard_EEG_fMRI( sidelength,trialTime,trialNum,blockNum,cbSize )
+% Input Params
+% sidelength: single box size
+% trialTime
+% trialNumber
+% blockNum
+% cbSize: ratio of checkerboard vs screen size, 1 full screen
+%
+% Huang Xiaoshan, xiaoshanhuang@gmail.com, 2012-3-4
+% Trigger Configer, Huang Xiaoshan, 20120331
+% Edited for BCG Artifact only, Huang Xiaoshan, 20120417
+
+if nargin < 1
+    shape = 'circ';
+end
+
+if nargin < 2
+    trialTime = 1;
+end
+    
+if nargin < 3
+    trialNum = 20;
+end
+
+if nargin < 4
+    blockNum = 6;
+end
+
+if nargin < 5
+    cbSize = 0.8;
+end
+
+totalTime = (trialTime*trialNum+10)*blockNum+10
+
+%% System config
+warning('off','MATLAB:dispatcher:InexactMatch');
+KbName('UnifyKeyNames');
+onExit='execution halted by experimenter';
+
+AssertOpenGL;
+
+%% Trigger Config
+triggerOut = false;
+
+if triggerOut,
+    config_io;
+    triggerPort = 'E800';
+    TRTrigger = 2;
+    stimTrigger = 1;
+    outp(hex2dec(triggerPort),0);
+end
+
+%%
+
+try
+    myScreen = max(Screen('Screens'));
+    [win,winRect] = Screen(myScreen,'OpenWindow');
+    Screen('TextSize', win, 100);
+    
+    [width, height] = RectSize(winRect);
+    Screen('FillRect',win,[0 0 0]);
+    
+    switch shape
+        case 'rect'
+            sidelength = 50;
+            numCheckers =  ceil([width; height] ./ sidelength);
+            % make an atomic checkerboard
+            miniboard = eye(2,'uint8') .* 255;
+            % repeat it in half of x,y, since it's 2x2
+            checkerboard_heads = repmat(miniboard, ceil(0.5 .* numCheckers))';
+            % invert for the other cycle
+            checkerboard_tails = 255 - checkerboard_heads;
+            % scale the images up
+            checkerboard_heads = imresize(checkerboard_heads,sidelength,'box');
+            checkerboard_tails = imresize(checkerboard_tails,sidelength,'box');
+        case 'circ'
+            % make circular checkerboard
+            sigma = 12;
+            spokes = 12;
+            SUP = height;
+            [checkerboard_heads, checkerboard_tails] = ...
+                make_circular_checkerboard_pattern(sigma,spokes,SUP);
+        otherwise
+            disp('Shape not supported');
+            return
+    end
+    % make textures clipped to screen size
+    blankSize = (1-cbSize)/2;
+    cropArea = round(height*blankSize):round(height*(1-blankSize));
+    texture(1) = Screen('MakeTexture', win, checkerboard_heads(cropArea,cropArea));
+    texture(2) = Screen('MakeTexture', win, checkerboard_tails(cropArea,cropArea));
+    % don't need those anymore
+    clear checkerboard_*; 
+    
+    % Define refresh rate.
+    ifi = Screen('GetFlipInterval', win);
+    
+    % set to 'ifi/2' for maximum refresh rate
+    % ifi/2 represents the next possible retrace
+    % 1/frequency < ifi is impossible
+%     swapinterval = 1 / frequency;
+%     deadline = GetSecs + trialNum*swapinterval;
+        
+    
+    Screen('DrawTexture',win,texture(1));
+    Screen('Flip', win);
+    Screen('DrawTexture',win,texture(2));
+    Screen('Flip', win, 0, 2);
+    % Wait for key press ('s') to start
+    startKey = KbName('s');
+    [keyIsDown, secs, keyCode] = KbCheck;
+    while ~keyCode(startKey)
+        [keyIsDown, secs, keyCode] = KbCheck;
+        assert(~keyCode(KbName('Escape')),onExit);
+    end
+    if triggerOut, outp(hex2dec(triggerPort),TRTrigger); end
+    
+    expStart = GetSecs;
+    %% Prestimulus Fixation
+%     WaitSecs(trialTime*trialNum);
+    WaitSecs(1);
+    if triggerOut, outp(hex2dec(triggerPort),0); end
+    for i = 1:blockNum
+        blockStart = GetSecs;
+        %% Flash
+        for j = 1:trialNum
+            if triggerOut, outp(hex2dec(triggerPort),TRTrigger); end
+            % Random
+            WaitSecs(0.05*(1+rand(1)));
+            if triggerOut, outp(hex2dec(triggerPort),0); end
+            Screen('Flip', win, 0, 2);
+            if triggerOut, outp(hex2dec(triggerPort),stimTrigger); end
+            while GetSecs-blockStart<trialTime*j,
+                [keyIsDown, secs, keyCode] = KbCheck;
+                assert(~keyCode(KbName('Escape')),onExit);
+            end
+            if triggerOut, outp(hex2dec(triggerPort),0); end
+        end
+        %% Fixation
+        while GetSecs-blockStart<trialTime*trialNum+10
+            [keyIsDown, secs, keyCode] = KbCheck;
+            assert(~keyCode(KbName('Escape')),onExit);
+        end
+    end
+    
+    totalTime = GetSecs - expStart
+    Screen('CloseAll');
+    disp('done');
+    warning('on','MATLAB:dispatcher:InexactMatch');
+
+
+catch
+
+    Screen('CloseAll');
+    psychrethrow(psychlasterror);
+
+end
+
+end
+
---
+++ Stimulus/make_circular_checkerboard_pattern.m
@@ -0,0 +1,32 @@
+function [f f_inv]=make_circular_checkerboard_pattern(sigma,spokes,SUP)
+% usage 
+% [f f_inv]=make_circular_checkerboard_pattern(2,4,500);
+% $author Shripad Kondra, NBRC, India (28-09-2010)
+% edited by Xiaoshan Huang, xiaoshanhuang@gmail.com
+if (nargin==0)
+sigma=4;
+spokes=6;
+SUP=500; % This parameter controls the resolution
+end
+hsup=(SUP-1)/2;
+[x,y]=meshgrid([-hsup:hsup]);
+[THETA,r] = cart2pol(x,y);
+r=(r./(SUP/2))*pi;
+% r(r<0.04)=0; % uncomment to put a dot at the centre.
+% r(r>(pi+0.01))=inf; % uncomment if you want to get exact circle
+%%
+f=sin(r*sigma);         % 1st concentric filter
+f1=sin(THETA*spokes);   % 1st radial filter
+f1=f1>=0;               % binarize
+f11=f.*f1;              % point-wise multiply
+f=sin(r*sigma+pi);      % 2nd concentric filter shifted by pi
+f1=sin(THETA*spokes+pi);% 2nd radial filter shifted by pi
+f1=f1>=0;               % binarize
+f12 = f.*f1;            % point-wise multiply
+f=(f11+f12)>=0;         % add the two filters and threshold
+f_inv=(f11+f12)<=0;     % add the two filters and threshold
+% scaled to 255;
+f = f.*255;
+f_inv = f_inv.*255;
+% figure, imagesc((f)), colormap gray;
+return
