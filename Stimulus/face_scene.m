6dc06aa45cfe4671a8b5f128904396177f2ae5d7
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-12-23 09:22:56 +0800

    clear stimTex

--- Stimulus/face_scene.m
+++ Stimulus/face_scene.m
@@ -314,6 +314,7 @@ try
                     Screen('Close', stimTex{cat, frame});
                 end
             end
+            clear stimTex;
         end
         % Block End
         if triggerOut, outp(hex2dec(triggerPort),trigBlockEnd+blk); end
