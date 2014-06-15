9d686fe930aabaf971661a2cd99db5119d98a3e2
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2014-06-15 13:02:32 +0800

    plot components

--- plotcomp.m
+++ plotcomp.m
@@ -1,8 +1,10 @@
-function plotcomp( C, z, chanlocs, times )
+function plotcomp( C, s, chanlocs, times )
 nComp = size(C,2);
+s = detrend(s')';
+s = s - repmat(mean(s(:,times<0),2),[1,size(s,2)]);
+s = s ./ repmat(std(s(:,times<0),0,2),[1,size(s,2)]);
 for iComp = 1:nComp
     subplot(2,nComp,iComp), topoplot(C(:,iComp),chanlocs);
 end
-subplot(2,nComp,nComp+1:2*nComp), plot(times,z');
+subplot(2,nComp,nComp+1:2*nComp), plot(times,s');
 end
-
